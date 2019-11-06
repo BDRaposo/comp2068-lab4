012decc0c0b611a929bb6d4ece2f94681cabebe4
Sphire <brandonraposo@msn.com>
2019-11-05 23:45:40 -0500

    Lab4 Complete

---
+++ ExpressApp5/routes/users.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var Account = require('../models/account');
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+//Get all accounts
+router.get('/users', isLoggedIn, function (req, res) {
+    Account.find(function (err, accounts) {
+        if (err) console.log(err);
+        else res.render('users', { allAccounts: accounts });
+    });
+});
+
+
+//Is the user logged in?
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    console.log('Not authenticated!');
+    res.redirect('/');
+}
+
+module.exports = router;
